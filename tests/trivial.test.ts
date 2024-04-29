090c51c979c927e0cd09e7a0b49ebf6042c6d382
Jeremiah Blanch <jeremiahblanch@gmail.com>
2024-04-29 12:41:38 +1000

    rename test to trivial

---
+++ tests/trivial.test.ts
@@ -0,0 +1,25 @@
+import SimpleDataProcessor from "../src";
+
+const myData = {'fieldName': 'value'};
+const theirData = {'field_name': 'value'};
+
+const sdp = new SimpleDataProcessor({
+  mine: {
+    fields: {
+      fieldName: 'field_name'
+    },
+  },
+  theirs: {
+    fields: {
+      field_name: 'fieldName'
+    },
+  }
+});
+
+test('correctly maps a simple object from theirs to mine', () => {
+  expect(sdp.convertToMine(theirData)).toStrictEqual(myData);
+});
+
+test('correctly maps a simple object from mine to theirs', () => {
+  expect(sdp.convertToTheirs(myData)).toStrictEqual(theirData);
+});
